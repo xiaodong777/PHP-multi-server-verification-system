1d4b27d935b90562dffe85ee1a9872657cb87dd4
xiaodong777 <99226604+xiaodong777@users.noreply.github.com>
2023-10-18 15:30:47 +0800

    Create blockipuser.php

---
+++ CSSJS/blockipuser.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>访问被阻止</title>
+</head>
+<body>
+    <h1>访问被阻止</h1>
+    <p>您的环境存在风险已经阻止，请公司管理员进行处理。</p>
+    <p>错误信息：O IP do usuário está bloqueado</p>
+</body>
+</html>
